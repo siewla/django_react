3a05ffaa4005824afee9adb776c6210ae5a0c301
siewla <siewlapang@gmail.com>
2021-05-04 20:17:49 +0800

    unique cart

--- shophooks/src/components/products/Product.jsx
+++ shophooks/src/components/products/Product.jsx
@@ -1,26 +1,40 @@
-import React from 'react'
+import React from "react";
 
-function Product({addCart, product, cart}) {
-    function addToCart(){
-        addCart(prevState=>[...prevState, product])
+function Product({ addCart, product, cart }) {
+  function addToCart() {
+    // console.log(cart)
+    // console.log(product)
+    if (!cart.includes(product)) {
+      addCart((prevState) => [...prevState, product]);
     }
 
-    function removeFromCart(){
-        // addCart(prevState=>[...prevState, product])
-        let index = cart.findIndex(el => el.id === product.id)
-        let tempCart = [...cart]
-        tempCart.splice(index, 1)
-        addCart(tempCart)
-    }
+    // let tempProduct = {...product}
+    // let index = cart.findIndex((el) => el.id === product.id);
+    // if (index >-1){
 
-    return (
-        <div>
-            {product.name}
-            <i>{product.price}</i>
-            <button onClick={addToCart}>Add To Cart</button>
-            <button onClick={removeFromCart}>Remove From Cart</button>
-        </div>
-    )
+    // }else{
+
+    // }
+  }
+
+  function removeFromCart() {
+    // addCart(prevState=>[...prevState, product])
+    if (cart.includes(product)) {
+      let index = cart.findIndex((el) => el.id === product.id);
+      let tempCart = [...cart];
+      tempCart.splice(index, 1);
+      addCart(tempCart);
+    }
+  }
+
+  return (
+    <div>
+      {product.name}
+      <i>{product.price}</i>
+      <button onClick={addToCart}>Add To Cart</button>
+      <button onClick={removeFromCart}>Remove From Cart</button>
+    </div>
+  );
 }
 
-export default Product
+export default Product;
--- shophooks/src/components/products/ProductList.jsx
+++ shophooks/src/components/products/ProductList.jsx
@@ -1,17 +1,19 @@
-import React from 'react'
-import Product from './Product'
+import React from "react";
+import Product from "./Product";
 
-function ProductList({addCart, products}) {
-    return (
-        <div>
-            {products.map(product => (
-                <Product 
-                addCart={addCart}
-                product={product} 
-                key={product.id} />
-            ))}
-        </div>
-    )
+function ProductList({ addCart, products, cart }) {
+  return (
+    <div>
+      {products.map((product) => (
+        <Product
+          addCart={addCart}
+          product={product}
+          cart={cart}
+          key={product.id}
+        />
+      ))}
+    </div>
+  );
 }
 
-export default ProductList
+export default ProductList;
